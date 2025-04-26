8d706f0105eadddf3f481c9055241bc6dcc6887c
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-04-26 23:35:00 +0100

    Update Azure Function settings and role assignments for Service Bus

--- terraform/function_app_role_assignments.tf
+++ terraform/function_app_role_assignments.tf
@@ -5,13 +5,13 @@ resource "azurerm_role_assignment" "app-to-storage" {
 }
 
 resource "azurerm_role_assignment" "app-to-servicebus-receiver" {
-  scope                = azurerm_servicebus_namespace.ingest.id
+  scope                = azurerm_servicebus_namespace.common_messaging.id
   role_definition_name = "Azure Service Bus Data Receiver"
   principal_id         = azurerm_linux_function_app.app.identity.0.principal_id
 }
 
 resource "azurerm_role_assignment" "app-to-servicebus-sender" {
-  scope                = azurerm_servicebus_namespace.ingest.id
+  scope                = azurerm_servicebus_namespace.common_messaging.id
   role_definition_name = "Azure Service Bus Data Sender"
   principal_id         = azurerm_linux_function_app.app.identity.0.principal_id
 }
